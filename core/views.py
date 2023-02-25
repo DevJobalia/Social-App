d361d9d42102082314aefc158b7e6c361976ae3a
Dev Jobalia <webdev.jobalia@gmail.com>
2023-02-25 12:30:06 +0530

    folllower functionality, custom login page

--- core/views.py
+++ core/views.py
@@ -146,11 +146,26 @@ def profile(request, pk):
     user_post = Post.objects.filter(user = pk)
     user_post_length = len(user_post)
 
+    follower = request.user.username
+    user = pk
+    
+    if followersCount.objects.filter(follower = follower, user = user).first():
+        button_text = "Unfollow"
+    else:
+        button_text = "Follow"
+    
+    user_follower = followersCount.objects.filter(user = user)
+    user_following = len(followersCount.objects.filter(follower = user))
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_post': user_post,
-        'user_post_length': user_post_length
+        'user_post_length': user_post_length,
+        'button_text': button_text,
+        'follower_count': user_follower.count(),
+        'following_count': user_following
+
     }
     return render(request, 'profile.html',context)
 
